b4e288f3d7a80f79bc09c8626a6d030fba0b414e
Dmitry <pchellovod@gmail.com>
2018-08-26 23:57:47 +0500

    add full_title method

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,12 +7,22 @@ describe "Static pages" do
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit '/'
       expect(page).to have_content('Sample App')
     end
 
+    it "should have the base title" do
+      visit '/'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/'
+      expect(page).not_to have_title('| Home')
+    end
+
     it "should have the title 'Home'" do
-      visit '/static_pages/home'
+      visit '/'
       expect(page).to have_title("#{base_title} | Home")
     end
   end
