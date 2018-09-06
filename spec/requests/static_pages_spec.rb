4b541c974c82e95343ad9905d3763b34ec034df0
dvoryankin <dvoryankindm@gmail.com>
2018-09-06 14:44:42 -0400

    refactoring home page spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,22 +4,14 @@ describe "Static pages" do
 
   let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
+  subject { page }
+
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the content 'App'" do
-      visit root_path
-      expect(page).to have_content('App')
-    end
-
-    it "should have the base title" do
-      visit root_path
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
-    end
-
-    it "should not have a custom page title" do
-      visit root_path
-      expect(page).not_to have_title('| Home')
-    end
+    it { should have_content('App') }
+    it { should have_title("Ruby on Rails Tutorial Sample App") }
+    it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
